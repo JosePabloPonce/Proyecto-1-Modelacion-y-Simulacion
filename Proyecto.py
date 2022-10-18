5a70947616be431d79db09f99eb676087a30df36
Gabriel Quiroz <46507325+gabrielquirozz@users.noreply.github.com>
2022-10-17 19:00:49 -0600

    Update Proyecto.py

--- Proyecto.py
+++ Proyecto.py
@@ -1,7 +1,5 @@
 import random
 import math
-from scipy.stats import poisson
-import numpy as np
 import simpy
 
 
@@ -13,6 +11,13 @@ tiempoFinal = 0
 CANTIDAD_CAJEROS = 5
 
 
+def poissonFunc(x, lambda_val):
+  return -(math.log(1 - x) / lambda_val)
+
+def DistExp(lambda_val):
+    return  - float((1/lambda_val)*math.log(1-random.random()))
+
+    
 class Cajero:
 
   CLIENTE_X_HORA = 20 #LAMBDA1
@@ -24,8 +29,7 @@ class Cajero:
     self.capacidad_cajeros = [0 for k in range(CANTIDAD_CAJEROS)] 
   
   def despacho_cliente(self, cliente):
-    beta = 1 / self.CLIENTE_X_HORA  #Beta = 1/Lambda
-    tiempo_despacho = np.random.exponential(beta)
+    tiempo_despacho = DistExp(self.CLIENTE_X_HORA)
     yield self.env.timeout(tiempo_despacho)  
     print('No. Simulacion: ' + str(self.env.now)[:4] + ' - Cliente ' + str(cliente) + ' despachado en ' + str(tiempo_despacho*60)[:4] +' min')
 
@@ -45,7 +49,6 @@ def client(env, client_id, Cajero):
     if Cajero.capacidad_cajeros[i] == min(Cajero.capacidad_cajeros):
       opcion_cajero.append(i)
 
-  ##########################################
   cajero_actual = random.choice(opcion_cajero)
 
   #Liberamos el recurso para poder solicitarlo nuevamente
@@ -56,45 +59,34 @@ def client(env, client_id, Cajero):
     yield req
     tiempo_finalizacion = env.now
     print('No. Simulacion: ' + str(env.now)[:4] + ' - Cliente ' + str(client_id) + ' esta siendo atendido por el cajero ' + str(cajero_actual))
-
     Cajero.capacidad_cajeros[cajero_actual] -= 1
-
-    
     yield env.process(Cajero.despacho_cliente(client_id))
     print('No. Simulacion: ' + str(env.now)[:4] + ' - Cliente ' + str(client_id) + ' fue atendido por el cajero ' + str(cajero_actual))
     tiempoFinal = env.now
     CLIENTES += 1
     tiempoEspera += tiempo_finalizacion - tiempo_inicio
 
-def poissonMedTime(x, lambda_val):
-  return -(math.log(1 - x) / lambda_val)
-
-
-# function that set uo the enviorment with the time intervals
 def setup(env, Cajero):
   client_id = 0
 
   while True:
-    CLIENTES_interval = poissonMedTime(random.random(), TOTAL_CLIENTES_HORA)
+    CLIENTES_interval = poissonFunc(random.random(), TOTAL_CLIENTES_HORA)
     yield env.timeout(CLIENTES_interval)
     env.process(client(env, client_id, Cajero))
     client_id += 1
 
-#Clean globals
 CLIENTES = 0
 tiempoEspera = 0
-caja_request = 0
-
 
 env = simpy.Environment()
-
 Cajero = Cajero(env)
-
 env.process(setup(env, Cajero))
 env.run(until=1)
 
-
+print("----------------------RESULTADOS DE LA SIMULACION-----------------------")
 print("Total de clientes: ", CLIENTES)
 print("Tiempo promedio de un cliente en la cola: ", tiempoEspera / CLIENTES)
-print("Numero de clientes en la cola por hora: ", (1 / (tiempoEspera / CLIENTES)) if tiempoEspera > 0 else 0)
+print("Numero de clientes en la cola por hora: ", (1 / (tiempoEspera / CLIENTES)))
 print("Grado de utilizacion de cada cajero: ", CLIENTES/CANTIDAD_CAJEROS)
+
+
